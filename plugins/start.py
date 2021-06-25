05277d62c88b6d5d9463284bc51dd772e237a2f8
CAt <80395140+SmokerCat@users.noreply.github.com>
2021-06-25 11:33:59 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -6,12 +6,12 @@ async def start(client, message):
     # return
     Lasiya = InlineKeyboardMarkup([
         
-        [InlineKeyboardButton("Creator ❤", url="https://t.me/Cat_of_telegramx")],
+        [InlineKeyboardButton("Youtube App ❤", url="https://www.youtube.com")],
         [InlineKeyboardButton(
-            "Support chat 😊", url="https://t.me/Cat_Telegram_Project_Club ")],
+            "Report Bugs 📋", url="https://t.me/Cat_Telegram_Project_Club")],
         [InlineKeyboardButton(
-            "Updates 🎛",url="https://t.me/Cat_Telegram_Projects")]
+            "Bot Updates ⚙",url="https://t.me/Cat_Telegram_Projects")]
     ])
     thumbnail_url = "https://telegra.ph/file/a488092b0f602ae43bbf0.jpg"
-    await message.reply_photo(thumbnail_url, caption=f"Hi<b>{message.from_user.first_name}</b>\n\n<b>Instructions for use..</b>\n• Type /help to get instructins.\n•───── ❝ **Lets Play** ❞ ─────\n ", reply_markup=Lasiya)
+    await message.reply_photo(thumbnail_url, caption=f"Hellow<b>{message.from_user.first_name}</b>\n\n<b>Instructions for use..</b>\n• Type /help to get instructins.\n• ───── ❝ **Lets Play** ❞ ─────\n ", reply_markup=Lasiya)
     raise StopPropagation

dea95916202c54cf653f78017581755cfe2a0ca6
CAt <80395140+SmokerCat@users.noreply.github.com>
2021-06-25 11:13:41 +0530

    Update tutorial.py

--- plugins/tutorial.py
+++ plugins/tutorial.py
@@ -2,9 +2,9 @@ from pyrogram import Client, Filters, StopPropagation, InlineKeyboardButton, Inl
 
 @Client.on_message(Filters.command(["tute"]))
 async def start(client, message):
-    await message.reply_text("To make a YouTube downloader pess ⚜️ Go ⚜️ button to go youtube directly. 🥳",
+    await message.reply_text("To Use @Yourtube_cat_bot Just do it. ⚜️ Go ⚜️ button to go to Tutorial.. 🥳",
                              reply_markup=InlineKeyboardMarkup(
         [
-            [InlineKeyboardButton("⚜️ Go ⚜️", url="https://youtu.be/l6rTppg3a1w")]
+            [InlineKeyboardButton("⚜️ Go ⚜️", url="https://t.me/Fallenflower_of_Telegram/11")]
         ]))
     

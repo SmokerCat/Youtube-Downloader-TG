3ef5b38f8a2437458703df0740d7be61b9cff336
CAt <80395140+SmokerCat@users.noreply.github.com>
2021-08-04 11:22:11 +0530

    Update help.py

--- plugins/help.py
+++ plugins/help.py
@@ -4,7 +4,7 @@ from pyrogram import Client, Filters, StopPropagation, InlineKeyboardButton, Inl
 @Client.on_message(Filters.command(["help"]))
 async def start(client, message):
 	Lasiya2 = InlineKeyboardMarkup([
-		[InlineKeyboardButton("Tutorial", url="https://t.me/Fallenflower_of_Telegram/11")],
+		[InlineKeyboardButton("ѕυρρσят", url="https://t.me/CatX_bot_hub")],
 		 ])
 	help_image = "https://telegra.ph/file/d87cfafd042561735d129.jpg"
-	await message.reply_photo(help_image,  caption="**♔Help**\n• Just Send your Youtube video url⛓ \n• And i will give Method list to select your choice😋",reply_markup=Lasiya2)
+	await message.reply_photo(help_image,  caption="**♔Help**\n• Copy Your Desired Video/Audio link from [Youtube](www.youtube.com) . \n• Send Link here..nd Select the Audio Quality from below buttons..After some time it get upload to here.. \n• For any help click below support button😊.. \n• Thank You For Using Our Service ",reply_markup=Lasiya2)
